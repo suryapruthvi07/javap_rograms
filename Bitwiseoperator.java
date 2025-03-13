5e92750f1588ec8237601b3335c5473cda463ad3
suryapruthvi07 <suryavanshipruthviraj881@gmail.com>
2025-03-13 17:35:27 +0530

    Add files via upload

---
+++ Bitwiseoperator.java
@@ -0,0 +1,21 @@
+package b;
+
+public class Bitwiseoperator {
+	
+	    public static void main(String[] args) {
+	        // Define the numbers
+	        int num1 = 73;
+	        int num2 = 69;
+	        int num3 = 89;
+
+	        // Perform the bitwise AND operation
+	        int result = num1 & num2 & num3;
+
+	        // Display the result
+	        System.out.println("The result of 73 & 69 & 89 is: " + result);
+	    }
+	}
+
+	
+
+
